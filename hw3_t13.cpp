ec7dba9b6a1fbae5785e8f9fb846764c6c5b0510
aphoric <63721060+aphoric@users.noreply.github.com>
2020-04-15 07:12:18 -0500

    Update hw3_t13.cpp

--- hw3_t13.cpp
+++ hw3_t13.cpp
@@ -1,3 +1,10 @@
+/*Team #13        CSCI/CMPE 1370       Spring 2020     Homework #3
+Jesus Guajardo
+Jacob Castro
+                     ~Description of Program~
+*/
+
+
 #include <iostream>
 using namespace std;
 int main(){
