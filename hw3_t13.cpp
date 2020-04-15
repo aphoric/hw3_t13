c3419453454bfa44abe0f63307e2a19372af15d2
aphoric <63721060+aphoric@users.noreply.github.com>
2020-04-15 12:56:04 -0500

    added main menu of calculator

--- hw3_t13.cpp
+++ hw3_t13.cpp
@@ -4,13 +4,19 @@ Jacob Castro
                      ~Description of Program~
 */
 
-
 #include <iostream>
 using namespace std;
 int main(){
+char option;
 
-  cout << "test" << endl;
+  cout << "Rational Numbers Calculator" << endl << endl;
+  cout << "(A)ddition" << endl;
+  cout << "(S)ubtraction" << endl;
+  cout << "(Q)uit" << endl << endl;
+  cout << "Enter your option: ";
+    cin >> option;
 
+cout << option;
 
 return 0;
 }
