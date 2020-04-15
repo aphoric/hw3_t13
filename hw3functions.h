cb82ad9f85e98f7c659c26bf58022b4beea5abf4
aphoric <63721060+aphoric@users.noreply.github.com>
2020-04-15 13:23:28 -0500

    added header file

---
+++ hw3functions.h
@@ -0,0 +1,7 @@
+void showMenu(){
+  cout << "Rational Numbers Calculator" << endl << endl;
+  cout << "(A)ddition" << endl;
+  cout << "(S)ubtraction" << endl;
+  cout << "(Q)uit" << endl << endl;
+  cout << "Enter your option: ";
+}
